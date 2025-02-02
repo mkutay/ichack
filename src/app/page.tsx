0fae00b55d796ae1a1e964eaf4b832ce079ce0c2
Andras Bard <91786910+AndrasBard@users.noreply.github.com>
2025-02-02 11:19:47 +0000

    Finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,26 @@ import { Button } from "@/components/ui/button";
 import { CreateExample } from "@/lib/db";
 
 export default function Page() {
+  const examples = [
+    "I'm considering studying Computer Science at university.",
+    "What time should I go to sleep?",
+    "I'm planning to found a startup."
+  ];
   return (
     <div className="md:max-w-6xl max-w-prose w-full mx-auto my-6 space-y-8 px-4">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
-        Placeholder.AI
+        PathfinderAI
       </h1>
       <div className="max-w-xl mx-auto">
         <FirstSubmitForm/>
         <div className="mt-6">
           First time? Here are some things you could ask me.
         </div>
-        <Button onClick={async () => await CreateExample("I'm thinking about studying computer science at university.")} className="px-4 py-3 my-4 w-full flex flex-col text-text bg-bw border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none items-center justify-start rounded-base ring-offset-white transition-all gap-3">
-          <div className="text-lg font-base text-left w-full leading-6 tracking-normal">I'm thinking about studying computer science at university.</div>
-        </Button>
-        <Button onClick={async () => await CreateExample('a')} className="px-4 py-3 my-4 w-full flex flex-col text-text bg-bw border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none items-center justify-start rounded-base ring-offset-white transition-all gap-3">
-          <div className="text-lg font-base text-left w-full leading-6 tracking-normal">Description</div>
-        </Button>
-        <Button onClick={async () => await CreateExample('a')} className="px-4 py-3 my-4 w-full flex flex-col text-text bg-bw border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none items-center justify-start rounded-base ring-offset-white transition-all gap-3">
-          <div className="text-lg font-base text-left w-full leading-6 tracking-normal">Description</div>
-        </Button>  
+        {examples.map(example => (
+          <Button onClick={async () => await CreateExample(example)} key={example} className="px-4 py-3 my-4 w-full flex flex-col text-text bg-bw border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none items-center justify-start rounded-base ring-offset-white transition-all gap-3">
+            <div className="text-lg font-base text-left w-full leading-6 tracking-normal">{example}</div>
+          </Button>
+        ))}
       </div>
     </div>
   );
