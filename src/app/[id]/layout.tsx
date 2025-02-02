1a83bf9e15f9b0cf51d56754b3f8a44b0b09b83a
Andras Bard <91786910+AndrasBard@users.noreply.github.com>
2025-02-02 06:53:33 +0000

    something

--- src/app/[id]/layout.tsx
+++ src/app/[id]/layout.tsx
@@ -12,9 +12,9 @@ export default function Layout(props: {
   const { id } = params;
 
   return (
-    <div className="flex flex-row mx-6 h-full">
+    <div className="flex flex-row h-full">
       {props.children}
-      <div className="min-h-screen w-1/3 py-6 pl-6 flex flex-col gap-6 justify-between bg-background border-l-2 border-border">
+      <div className="overflow-auto max-h-screen w-1/3 py-6 px-6 flex flex-col gap-6 justify-between bg-background border-l-2 border-border">
         <div className="space-y-4 w-full">
           <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
             Info pages, question and answers
